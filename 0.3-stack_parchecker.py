9183cdc25080eea7c10a7558a946b18f108af0e2
YuanMing Liu <774166816@qq.com>
2017-07-31 23:54:18 +0800

    modify

--- 0.3-stack_parchecker.py
+++ 0.3-stack_parchecker.py
@@ -1,4 +1,6 @@
 from pythonds.basic.stack import Stack
+
+# 问题1: 简单括号"()"匹配。
 def parchecker(symbolString):
     s = Stack()
     balanced = True
@@ -22,3 +24,35 @@ def parchecker(symbolString):
 
 print(parchecker('((()))'))
 print(parchecker('(()'))
+
+
+# 问题2: 符号匹配"([{"。
+def matches(open, close):
+    opens = "([{"
+    closers = ")]}"
+    return opens.index(open) == closers.index(close)
+
+def parchecker_pro(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+print(parchecker_pro('{{([][])}()}'))
+print(parchecker_pro('[{()]'))
